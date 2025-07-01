97814489104166a05edcec4c490c064e16cd289b
Chinami0702 <chinamik0702@gmail.com>
2025-07-01 09:46:50 +0900

    インデント

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,28 +1,28 @@
 //hunberger-menu
 $(function () {
-  $('#js-hamburger-menu, .header__link').on('click', function () {
-    $('.header__nav').slideToggle(500)
-    $('.hamburger-menu').toggleClass('hamburger-menu--open')
-  });
+    $('#js-hamburger-menu, .header__link').on('click', function () {
+        $('.header__nav').slideToggle(500)
+        $('.hamburger-menu').toggleClass('hamburger-menu--open')
+    });
 });
 //garlley
 const swiper = new Swiper('.swiper', {
     loop: true,
     autoplay: {
-      delay: 100000,
+        delay: 100000,
     },
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
     },
-  });
-  //３枚目qa
-  $(document).ready(function () {
-  $('.qa__toggle').on('click', function () {
-    const $answer = $(this).closest('.qa__box').find('.qa__answer');
-    const isVisible = $answer.is(':visible');
+});
+//３枚目qa
+$(document).ready(function () {
+    $('.qa__toggle').on('click', function () {
+        const $answer = $(this).closest('.qa__box').find('.qa__answer');
+        const isVisible = $answer.is(':visible');
 
-    $answer.slideToggle(300);
-    $(this).text(isVisible ? '＋' : '−');
-  });
+        $answer.slideToggle(300);
+        $(this).text(isVisible ? '＋' : '−');
+    });
 });
