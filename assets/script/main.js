0aecc3da7cf14d26490c69b24d8086c2613af6ad
Chinami0702 <chinamik0702@gmail.com>
2025-07-01 14:58:43 +0900

    改善

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,11 @@
-//hunberger-menu
+
 $(function () {
     $('#js-hamburger-menu, .header__link').on('click', function () {
         $('.header__nav').slideToggle(500)
         $('.hamburger-menu').toggleClass('hamburger-menu--open')
     });
 });
-//garlley
+
 const swiper = new Swiper('.swiper', {
     loop: true,
     autoplay: {
@@ -16,10 +16,10 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
 });
-//３枚目qa
+
 $(document).ready(function () {
     $('.qa__toggle').on('click', function () {
-        const $answer = $(this).closest('.qa__box').find('.qa__answer');
+        const $answer = $(this).closest('.qa__wrapper').find('.qa__answer');
         const isVisible = $answer.is(':visible');
 
         $answer.slideToggle(300);
