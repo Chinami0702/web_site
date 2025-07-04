112433d37a4bd6e2565c533d693af3672259467e
Chinami0702 <chinamik0702@gmail.com>
2025-07-04 22:04:56 +0900

    最終

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,11 @@
-
+//ハンバーガーメニュー
 $(function () {
     $('#js-hamburger-menu, .header__link').on('click', function () {
         $('.header__nav').slideToggle(500)
         $('.hamburger-menu').toggleClass('hamburger-menu--open')
     });
 });
-
+//写真
 const swiper = new Swiper('.swiper', {
     loop: true,
     autoplay: {
@@ -16,16 +16,7 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
 });
-
-// $(document).ready(function () {
-//     $('.qa__toggle').on('click', function () {
-//         const $answer = $(this).closest('.qa__wrapper').find('.qa__answer');
-//         const isVisible = $answer.is(':visible');
-
-//         $answer.slideToggle(300);
-//         $(this).text(isVisible ? '＋' : '−');
-//     });
-// });
+//質問
 $(document).ready(function () {
     $('.qa__wrapper').on('click', function () {
         const $wrapper = $(this);
